b9d5e83b521d974b7f31e4a15b6b5c331a51cd66
MariaBarros02 <141078532+MariaBarros02@users.noreply.github.com>
2023-09-12 07:57:15 -0500

    Update script01.js

--- script01.js
+++ script01.js
@@ -1,8 +1,15 @@
 
+
 function cambiarContenido() {
-    let contenido = document.getElementById("botonCambiarContenido");
-    contenido.innerText= "Lorem ipsum";
+
+    let contenido = document.getElementById("texto");
+    contenido.innerText =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis purus sit amet volutpat. Et magnis dis parturient montes nascetur ridiculus mus mauris vitae. Massa sed elementum tempus egestas sed. Neque viverra justo nec ultrices dui sapien eget mi. Orci nulla pellentesque dignissim enim sit amet. Viverra nam libero justo laoreet sit amet cursus. Diam donec adipiscing tristique risus nec feugiat in. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Tempus urna et pharetra pharetra massa. Diam vel quam elementum pulvinar etiam non quam. Urna porttitor rhoncus dolor purus non enim. Odio aenean sed adipiscing diam donec adipiscing tristique risus nec. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Pharetra sit amet aliquam id diam maecenas ultricies mi eget. Arcu cursus euismod quis viverra nibh cras pulvinar.";
 }
-function cambiarColor(){
+function cambiarColor() {
+    let texto = document.getElementById("texto");
+    texto.style.color = "#272727";
+    let pagina = document.getElementById("body");
+    pagina.style.backgroundColor = "#c4c4c4";
 
 }
